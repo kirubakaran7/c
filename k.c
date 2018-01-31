369aeaa9069cfc22c514f1453025b09fa0a3a0b8
kirubakaran7 <35688321+kirubakaran7@users.noreply.github.com>
2018-01-31 14:43:41 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	char ch;
+	printf("Enter an alphabet : ");
+	scanf("%c",&ch);
+	{
+		printf("This is a vowel");
+	}
+	else
+	{
+		printf("This is not a vowel");
+	}
+	getch();
+}
