f6a171c74acc8ba6416cb3a6a9aeb0a834fac701
kirubakaran7 <35688321+kirubakaran7@users.noreply.github.com>
2018-01-31 14:21:19 +0530

    Create kiruba.C

---
+++ kiruba.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+printf("the number is odd");
+else
+printf("the number is even");
+getch();
+}
