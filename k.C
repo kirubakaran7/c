ff67a55f636641dbc5aad480dce6729759df83ee
kirubakaran7 <35688321+kirubakaran7@users.noreply.github.com>
2018-01-31 14:31:18 +0530

    Create k.C

---
+++ k.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("enter a number",num);
+scanf("%d",&num);
+printf("\n number is positive");
+else
+printf("\n number is negative");
+getch();
+}
