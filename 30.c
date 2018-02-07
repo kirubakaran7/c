e825ca5b2b0635018d77ada32248724b7d1d6bbb
kirubakaran7 <35688321+kirubakaran7@users.noreply.github.com>
2018-02-07 14:28:04 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+void main()
+{
+int h1,h2,m1,m2,a,b;
+printf("\nEnter h1 and m1:");
+scanf("%d%d",&h1,&m1);
+printf("\nEnter h2 and m2:");
+scanf("%d%d",&h2,&m2);
+a=h1-h2;
+b=m1-m2;
+printf("%d %d",a,b);
+}
