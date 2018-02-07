cc529a8acca8045f9d4cb0de43170b36d0f1319b
kirubakaran7 <35688321+kirubakaran7@users.noreply.github.com>
2018-02-07 14:14:47 +0530

    Rename 27.c to 28.c

---
+++ 28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int a[25],n,i;
+ printf("Enter how many elemens:");
+ scanf("%d",&n);
+ printf("Enter the array elements:");
+ scanf("%d",&a[i]);
+ for(i=0;i<n;i++)
+  {
+  printf("\n%d %d\n",a[i],i);
+  }
+ return 0;
+}
