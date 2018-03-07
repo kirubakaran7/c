cb7dfbe95c05cda099cd31d917399407eca227fb
kirubakaran <35688321+kirubakaran7@users.noreply.github.com>
2018-03-07 14:10:24 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,m;
+   printf("enter the number:\n");
+   scanf("%d %d",&a,&b);
+   m=a*b;
+   if(a==b)
+    {
+      printf("yes");
+    }
+   else
+    {
+       printf("no");
+    }
+    return 0;
+}
