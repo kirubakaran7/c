6b7217555692be5688a36801ce4beb898a892721
kirubakaran <35688321+kirubakaran7@users.noreply.github.com>
2018-03-09 15:55:57 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,11 @@
+include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char a;
+ printf("\n enter the character",a);
+ scanf("%c",&a);
+ printf("the  ascii value is");
+ scanf("%d",&a);
+ getch();
+}
